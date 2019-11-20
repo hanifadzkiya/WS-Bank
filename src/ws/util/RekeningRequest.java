eca743e7cd98f89f529d37e6ffa324f7b18e5f1b
hanif adzkiya <hanifadzkiya@gmail.com>
2019-11-20 16:57:26 +0700

    Edit RekeningRequest

--- src/ws/util/RekeningRequest.java
+++ src/ws/util/RekeningRequest.java
@@ -2,6 +2,8 @@ package ws.util;
 
 public class RekeningRequest {
     private String noRekening;
+    private int idNasabah;
+
 
     public RekeningRequest(RekeningRequestBuilder rekeningRequestBuilder){
         this.noRekening = rekeningRequestBuilder.getNoRekening();
